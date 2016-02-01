936a20a110ae2e8071bc870acc7a024ac2cf44d8
joegotflow83 <joe@absolutod.com>
2016-02-01 14:07:59 -0500

    Added ability to 'unpiggify' word

--- main.py
+++ main.py
@@ -1,9 +1,73 @@
 #!/usr/bin/env python3.5
 
 
-def pick_word():
+def begin():
 
-	word = input("Type a word: ")
+	choice = input("Do you want to [P]ig a word or [U]npig a word "
+				 "Type a letter in the brackets: ").lower()
+
+	if choice == 'p':
+
+		pig_word()
+
+	elif choice == 'u':
+
+		unpig_word()
+
+	else:
+
+		print("I am sorry, what did you say? Type either p or u   ")
+		begin()
+
+def pig_word():
+
+	pick_pig_word()
+
+def unpig_word():
+
+	pick_unpig_word()
+
+def single_translate(word):
+
+	vowels = ['a', 'e', 'i', 'o', 'u']
+
+	if word[0] in vowels:
+
+		word = word.replace(word[0], '')
+		new_word = word + 'say'
+		print(new_word)
+
+	else:
+
+		first_letter = word[0]
+		word = word.replace(word[0], '')
+		new_word = word + last_letter + 'ay'
+		print(new_word)
+
+def multiple_translate(words):
+
+	for word in words:
+
+		single_translate(word)
+
+def single_reverse(word):
+
+	new_word = word[0:(len(word) -2)]
+	last_letter = new_word[-1]
+	new_word = new_word.replace(new_word[-1], '')
+	new_word = last_letter + new_word
+	print(new_word)
+
+def multiple_reverse(words):
+
+	for word in words:
+
+		single_reverse(word)
+
+def pick_pig_word():
+
+	word = input("Type a word to pig out: ")
+
 	word = word.split(' ')
 	
 	if len(word) == 1:
@@ -15,27 +79,19 @@ def pick_word():
 
 		multiple_translate(word)
 
-def single_translate(word):
+def pick_unpig_word():
 
-	vowels = ['a', 'e', 'i', 'o', 'u']
+	word = input("Type a word to unpig out: ")
 
-	if word[0] in vowels:
+	word = word.split(' ')
+	
+	if len(word) == 1:
 
-		word = word.replace(word[0], '')
-		new_word = word + 'ay'
-		print(new_word)
+		word = ''.join(word)
+		single_reverse(word)
 
 	else:
 
-		last_letter = word[0]
-		word = word.replace(word[0], '')
-		new_word = word + last_letter + 'ay'
-		print(new_word)
+		multiple_reverse(word)
 
-def multiple_translate(words):
-
-	for word in words:
-
-		single_translate(word)
-
-pick_word()
+begin()
